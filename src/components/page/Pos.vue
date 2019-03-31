e7526af7b02867a7b9b85c6566af93f463450b53
springleeo@163.com <springleeo@springleeodeMacBook-Pro.local>
2019-03-31 22:11:55 +0800

    页面布局1

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -1,36 +1,78 @@
 <template>
-    <div class="Pos">
-        <le-row>
-            <el-col :span='7' class="pos-order" id='order-list'>
-                我是订单栏
-            </el-col>
-            <el-col>
-                我是产品栏
-            </el-col>
-        </le-row>
-    </div>
+  <div class="Pos">
+    <le-row>
+      <el-col :span="7" class="pos-order" id="order-list">
+        <el-tabs>
+          <el-tab-pane label="点餐">
+            <el-table :data="tableData" border style="width:100%">
+              <el-table-column prop="goodsName" label="商品名称"></el-table-column>
+              <el-table-column prop="count" label="数量"></el-table-column>
+              <el-table-column prop="price" label="金额"></el-table-column>
+              <el-table-column label="操作" fixed="right">
+                <template>
+                  <el-button type="text" size="samll">删除</el-button>
+                  <el-button type="text" size="samll">添加</el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+            <div class="div-btn">
+              <el-button type="warning">挂单</el-button>
+              <el-button type="danger">删除</el-button>
+              <el-button type="success">结账</el-button>
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="挂单">挂单</el-tab-pane>
+          <el-tab-pane label="外卖">外卖</el-tab-pane>
+        </el-tabs>
+      </el-col>
+      <el-col>我是产品栏</el-col>
+    </le-row>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'Pos',
-    data(){
-        return{
-
+  name: "Pos",
+  data() {
+    return {
+      tableData: [
+        {
+          goodsName: "可口可乐",
+          price: 8,
+          count: 1
+        },
+        {
+          goodsName: "香辣鸡腿堡",
+          price: 15,
+          count: 1
+        },
+        {
+          goodsName: "爱心薯条",
+          price: 8,
+          count: 1
+        },
+        {
+          goodsName: "甜筒",
+          price: 8,
+          count: 1
         }
-    },
-    mounted:function(){
-        var orderHeight=document.body.clientHeight;
-        console.log(orderHeight)
-        document.getElementById('order-list').style.height=orderHeight+'px'
-    }
-}
+      ]
+    };
+  },
+  mounted: function() {
+    var orderHeight = document.body.clientHeight;
+    console.log(orderHeight);
+    document.getElementById("order-list").style.height = orderHeight + "px";
+  }
+};
 </script scoped>
 
 <style>
-.pos-order{
-    background-color: #f9fafc;
-    border-right: 1px solid #c0ccda;
-
+.pos-order {
+  background-color: #f9fafc;
+  border-right: 1px solid #c0ccda;
+}
+.div-btn {
+  margin-top: 10px;
 }
 </style>
