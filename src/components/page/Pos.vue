384b27f62310159e52a2f3f5be2213ff3253e1e4
springleeo <springleeo@163.com>
2019-04-02 14:42:17 +0800

    axios

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -52,9 +52,45 @@
                 </ul>
               </div>
             </el-tab-pane>
-            <el-tab-pane label="小食"></el-tab-pane>
-            <el-tab-pane label="饮料"></el-tab-pane>
-            <el-tab-pane label="套餐"></el-tab-pane>
+            <el-tab-pane label="小食">
+              <div>
+                <ul class="cookList">
+                  <li v-for="goods in type1Goods">
+                    <span class="foodImg">
+                      <img :src="goods.goodsImg">
+                    </span>
+                    <span class="foodName">{{goods.goodsName}}</span>
+                    <span class="foodPrice">￥{{goods.price}}元</span>
+                  </li>
+                </ul>
+              </div>
+            </el-tab-pane>
+            <el-tab-pane label="饮料">
+              <div>
+                <ul class="cookList">
+                  <li v-for="goods in type2Goods">
+                    <span class="foodImg">
+                      <img :src="goods.goodsImg">
+                    </span>
+                    <span class="foodName">{{goods.goodsName}}</span>
+                    <span class="foodPrice">￥{{goods.price}}元</span>
+                  </li>
+                </ul>
+              </div>
+            </el-tab-pane>
+            <el-tab-pane label="套餐">
+              <div>
+                <ul class="cookList">
+                  <li v-for="goods in type3Goods">
+                    <span class="foodImg">
+                      <img :src="goods.goodsImg">
+                    </span>
+                    <span class="foodName">{{goods.goodsName}}</span>
+                    <span class="foodPrice">￥{{goods.price}}元</span>
+                  </li>
+                </ul>
+              </div>
+            </el-tab-pane>
           </el-tabs>
         </div>
       </el-col>
@@ -63,6 +99,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: "Pos",
   data() {
@@ -90,107 +127,38 @@ export default {
         }
       ],
       offenGoods: [
-        {
-          goodsId: 1,
-          goodsName: '香辣鸡腿堡',
-          price: 18
-        }, {
-          goodsId: 2,
-          goodsName: '田园鸡腿堡',
-          price: 15
-        }, {
-          goodsId: 3,
-          goodsName: '和风汉堡',
-          price: 15
-        }, {
-          goodsId: 4,
-          goodsName: '快乐全家桶',
-          price: 80
-        }, {
-          goodsId: 5,
-          goodsName: '脆皮炸鸡腿',
-          price: 10
-        }, {
-          goodsId: 6,
-          goodsName: '魔法鸡块',
-          price: 20
-        }, {
-          goodsId: 7,
-          goodsName: '可乐大杯',
-          price: 10
-        }, {
-          goodsId: 8,
-          goodsName: '雪顶咖啡',
-          price: 18
-        }, {
-          goodsId: 9,
-          goodsName: '大块鸡米花',
-          price: 15
-        }, {
-          goodsId: 20,
-          goodsName: '香脆鸡柳',
-          price: 17
-        }
       ],
-      type0Goods: [
-        {
-          goodsId: 1,
-          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
-          goodsName: '香辣鸡腿堡',
-          price: 18
-        }, {
-          goodsId: 2,
-          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
-          goodsName: '田园鸡腿堡',
-          price: 15
-        }, {
-          goodsId: 3,
-          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos004.jpg",
-          goodsName: '和风汉堡',
-          price: 15
-        }, {
-          goodsId: 4,
-          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
-          goodsName: '快乐全家桶',
-          price: 80
-        }, {
-          goodsId: 5,
-          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
-          goodsName: '脆皮炸鸡腿',
-          price: 10
-        }, {
-          goodsId: 6,
-          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos004.jpg",
-          goodsName: '魔法鸡块',
-          price: 20
-        }, {
-          goodsId: 7,
-          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
-          goodsName: '可乐大杯',
-          price: 10
-        }, {
-          goodsId: 8,
-          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
-          goodsName: '雪顶咖啡',
-          price: 18
-        }, {
-          goodsId: 9,
-          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
-          goodsName: '大块鸡米花',
-          price: 15
-        }, {
-          goodsId: 20,
-          goodsImg: "http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
-          goodsName: '香脆鸡柳',
-          price: 17
-        }
-      ]
+      type0Goods: [],
+      type1Goods: [],
+      type2Goods: [],
+      type3Goods: [],
+
     };
   },
   mounted: function () {
     var orderHeight = document.body.clientHeight;
     console.log(orderHeight);
     document.getElementById("order-list").style.height = orderHeight + "px";
+  },
+  created: function () {
+    axios.get('https://www.easy-mock.com/mock/5b8b30dbf032f03c5e71de7f/kuaican/oftenGoods')
+      .then(res => {
+        this.offenGoods = res.data;
+      })
+      .catch(error => {
+        alert('网络错误')
+      })
+
+    axios.get('https://www.easy-mock.com/mock/5b8b30dbf032f03c5e71de7f/kuaican/typeGoods')
+      .then(res => {
+        this.type0Goods = res.data[0];
+        this.type1Goods = res.data[1];
+        this.type2Goods = res.data[2];
+        this.type3Goods = res.data[3];
+      })
+      .catch(error => {
+        alert('网络错误')
+      })
   }
 };
 </script scoped>
